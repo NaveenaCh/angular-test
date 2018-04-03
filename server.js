129ed97447001a269b24aa398c0be7d2d1d894b2
NaveenaCh <naveena.chokkara@quantela.com>
2018-04-03 11:33:18 +0530

    added logs to server file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+try { //Install express server
+    var express = require('express');
+    var app = express();
 
-//Install express server
-var express = require('express');
-var app = express();
+    // Serve only the static files form the dist directory
+    app.use(express.static(__dirname + '/dist'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+    // Start the app by listening on the default Heroku port
+    app.listen(process.env.PORT || 8080);
+    console.log("app server is listening at 8080");
+} catch (e) {
+    console.log("server error" + e)
+}
